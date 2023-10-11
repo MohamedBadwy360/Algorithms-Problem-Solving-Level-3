7746ff17cc9304d9c5302cad73a660f6c883e879
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2023-10-11 11:10:42 +0300

    Random Matrix

---
+++ 1. Random Matrix/1. Random Matrix.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <cstdlib>
+#include <iomanip>
+
+using namespace std;
+
+int random_number(int from, int to)
+{
+    return (rand() % (to - from + 1) + from);
+}
+
+void fill_matrix_with_random_number(int arr[3][3], short rows, short cols)
+{
+    for (short i = 0; i < rows; i++)
+    {
+        for (short j = 0; j < cols; j++)
+        {
+            arr[i][j] = random_number(1, 100);
+        }
+    }
+}
+
+void print_matrix(int arr[3][3], short rows, short cols)
+{
+    for (short i = 0; i < rows; i++)
+    {
+        for (short j = 0; j < cols; j++)
+        {
+            cout << setw(3) << arr[i][j] << " ";
+        }
+
+        cout << endl;
+    }
+}
+
+int main()
+{
+    srand(unsigned(time(NULL)));
+
+    int arr[3][3];
+    fill_matrix_with_random_number(arr, 3, 3);
+
+    cout << "The following is random matrix: \n";
+    print_matrix(arr, 3, 3);
+
+    return 0;
+}
